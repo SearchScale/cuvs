a6ae4ae3da82d77d02263c14e61ee2276c15d6af
Ishan Chattopadhyaya <ishan@apache.org>
2024-11-26 19:02:04 +0530

    Tensors should be ndim=2

--- java/internal/src/cuvs_java.c
+++ java/internal/src/cuvs_java.c
@@ -32,7 +32,7 @@ DLManagedTensor prepare_tensor(void *data, int64_t shape[], DLDataTypeCode code,
 
   tensor.dl_tensor.data = data;
   tensor.dl_tensor.device.device_type = kDLCUDA;
-  tensor.dl_tensor.ndim = dimensions;
+  tensor.dl_tensor.ndim = 2;
   tensor.dl_tensor.dtype.code = code;
   tensor.dl_tensor.dtype.bits = 32;
   tensor.dl_tensor.dtype.lanes = 1;
