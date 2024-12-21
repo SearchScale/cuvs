0c7c73b10fb5c5de2a2a25cbb6a36ac15380f048
Vivek Narang <vivek@searchscale.com>
2024-12-20 21:31:44 -0500

    revert skip tests flag

--- java/build.sh
+++ java/build.sh
@@ -8,5 +8,5 @@ cd internal && cmake . && cmake --build . \
   && cd .. \
   && mvn install:install-file -DgroupId=$GROUP_ID -DartifactId=cuvs-java-internal -Dversion=$VERSION -Dpackaging=so -Dfile=$SO_FILE_PATH/libcuvs_java.so \
   && cd cuvs-java \
-  && mvn -DskipTests=true package \
+  && mvn package \
   && mvn install:install-file -Dfile=./target/cuvs-java-$VERSION-jar-with-dependencies.jar -DgroupId=$GROUP_ID -DartifactId=cuvs-java -Dversion=$VERSION -Dpackaging=jar
